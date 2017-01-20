3a02aa6fa567903c6fbad88c7d3d550781cf72d7
yuchonghua <yuchonghua@163.com>
2017-01-20 23:57:40 +0800

    JavaScript Standard Style
    切换js标准

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-'use strict';
-let type = (process.env.DDV_WORKER_PROCESS_TYPE||'worker');
+'use strict'
+let type = (process.env.DDV_WORKER_PROCESS_TYPE || 'worker')
 
-//转小写
-type = (type || 'daemon').toString().toLowerCase();
-//强制是以下类型
-type = ['master', 'worker', 'daemon'].indexOf(type)>-1 ? type : 'daemon';
-//引入实例化基础类
-const DdvWorker = require('./lib/DdvWorker.js');
-//设定类型
-DdvWorker.DDV_WORKER_PROCESS_TYPE = type ;
+// 转小写
+type = (type || 'daemon').toString().toLowerCase()
+// 强制是以下类型
+type = ['master', 'worker', 'daemon'].indexOf(type) > -1 ? type : 'daemon'
+// 引入实例化基础类
+const DdvWorker = require('./lib/DdvWorker.js')
+// 设定类型
+DdvWorker.DDV_WORKER_PROCESS_TYPE = type
 
-if (['master', 'worker'].indexOf(type)>-1) {
-	module.exports = new DdvWorker();
-}else{
-	module.exports = DdvWorker;
+if (['master', 'worker'].indexOf(type) > -1) {
+	module.exports = new DdvWorker()
+} else {
+	module.exports = DdvWorker
 }
-//暴露实例化对象
-module.exports.DdvWorker = DdvWorker;
-module.exports.now = DdvWorker.prototype.now;
-module.exports.time = DdvWorker.prototype.time;
-module.exports.createNewid = DdvWorker.prototype.createNewid;
-module.exports.type = DdvWorker.prototype.type;
-module.exports.isFunction = DdvWorker.prototype.isFunction;
-module.exports.isArray = DdvWorker.prototype.isArray;
-module.exports.deepClone = DdvWorker.prototype.deepClone;
-module.exports.argsToArray = DdvWorker.prototype.argsToArray;
-module.exports.defineGetter = DdvWorker.prototype.defineGetter;
-module.exports.defineSetter = DdvWorker.prototype.defineSetter;
+// 暴露实例化对象
+module.exports.DdvWorker = DdvWorker
+module.exports.now = DdvWorker.prototype.now
+module.exports.time = DdvWorker.prototype.time
+module.exports.createNewid = DdvWorker.prototype.createNewid
+module.exports.type = DdvWorker.prototype.type
+module.exports.isFunction = DdvWorker.prototype.isFunction
+module.exports.isArray = DdvWorker.prototype.isArray
+module.exports.deepClone = DdvWorker.prototype.deepClone
+module.exports.argsToArray = DdvWorker.prototype.argsToArray
+module.exports.defineGetter = DdvWorker.prototype.defineGetter
+module.exports.defineSetter = DdvWorker.prototype.defineSetter
